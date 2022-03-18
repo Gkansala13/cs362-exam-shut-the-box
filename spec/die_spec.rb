d4cd4b9248a2bb663ad59a555d02b54073275395
gkansala13 <kansalag@oregonstate.edu>
2022-03-17 20:18:18 -0700

    Completed all unit tests besides the rolling test
    Refrence #4

--- spec/die_spec.rb
+++ spec/die_spec.rb
@@ -16,23 +16,27 @@ describe 'A die' do
 
   describe 'number of sides' do
     it 'has six sides by default' do
-      
+      die=Die.new
+      expect(die.number_of_sides).to eq(6)
     end
 
     it 'can be created with a custom number of sides' do
-      
+      die=Die.new(1)
+      expect(die.number_of_sides).to eq(1)
     end
   end
 
   describe 'face value' do
     it 'has a face value of 1 by default' do
-      
+      die=Die.new
+      expect(die.face_value).to eq(1)
     end
   end
 
   describe '#to_s string representation' do
     it 'tunrs facevalue into string' do
-      
+      die=Die.new
+      expect(die.to_s).to eq('1')
     end
   end
 
