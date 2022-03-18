69dbb0b2868b508ba2dbd8545b8689225ff105af
gkansala13 <kansalag@oregonstate.edu>
2022-03-17 20:17:04 -0700

    Added attribute tests for die
    reference #3

--- spec/die_spec.rb
+++ spec/die_spec.rb
@@ -3,34 +3,36 @@ require_relative '../lib/die'
 describe 'A die' do
 
   describe 'attributes' do
-    it 'TODO' do
-      skip
+    it 'Has number of sides' do
+      die=Die.new
+      expect(die).to respond_to(:number_of_sides)
     end
 
-    it 'TODO' do
-      skip
+    it 'Has face value' do
+      die=Die.new
+      expect(die).to respond_to(:face_value)
     end
   end
 
   describe 'number of sides' do
     it 'has six sides by default' do
-      skip
+      
     end
 
     it 'can be created with a custom number of sides' do
-      skip
+      
     end
   end
 
   describe 'face value' do
     it 'has a face value of 1 by default' do
-      skip
+      
     end
   end
 
   describe '#to_s string representation' do
-    it 'TODO' do
-      skip
+    it 'tunrs facevalue into string' do
+      
     end
   end
 
